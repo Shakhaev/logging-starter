0f6f8fe731898d5989c0f13042e2375b2fff4a9f
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2025-02-10 14:08:23 +0300

    Initial commit

---
+++ src/main/java/ru/t1/spring/logging/starter/LoggingStarterApplication.java
@@ -0,0 +1,13 @@
+package ru.t1.spring.logging.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingStarterApplication.class, args);
+	}
+
+}
